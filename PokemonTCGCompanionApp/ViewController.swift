9e79b3ffdea27b2614e7fb3037abbd70e12a9ffe
Curtis Pritchard <cpritchard649@gmail.com>
2020-10-13 20:05:47 -0400

    Initial Commit

---
+++ PokemonTCGCompanionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonTCGCompanionApp
+//
+//  Created by Curtis Pritchard on 2020-10-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
