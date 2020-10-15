223e6ceb273f9227cb121fe6fcdf36de621d7c26
Curtis Pritchard <cpritchard649@gmail.com>
2020-10-15 13:16:38 -0400

    removed searchPage search bar, replacing with embedded UISearchController. =

---
+++ PokemonTCGCompanionApp/ViewControllers/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewController.swift
+//  PokemonTCGCompanionApp
+//
+//  Created by Curtis Pritchard on 2020-10-15.
+//
+
+import UIKit
+
+
+class SearchViewController: UIViewController {
+    
+    //TODO: add hidable UISearchController into the pages CollectionView with Scope bar [Pokemon, Stadium, Trainer]
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
