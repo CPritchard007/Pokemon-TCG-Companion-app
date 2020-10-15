8545da25596d6904fc10033d864c7808dad29ed8
Curtis Pritchard <cpritchard649@gmail.com>
2020-10-15 14:24:00 -0400

    created searchbar for collectionView

--- PokemonTCGCompanionApp/ViewControllers/SearchViewController.swift
+++ PokemonTCGCompanionApp/ViewControllers/SearchViewController.swift
@@ -8,13 +8,28 @@
 import UIKit
 
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UISearchResultsUpdating {
+    //MARK: - Properties
+    let searchController = UISearchController(searchResultsController: nil)
     
-    //TODO: add hidable UISearchController into the pages CollectionView with Scope bar [Pokemon, Stadium, Trainer]
+    
+ 
+    //MARK: - Outlets
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //TODO: add hidable UISearchController into the pages CollectionView with Scope bar [Pokemon, Energy, Trainer]
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "ENTER CARD NAME"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+    }
+
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        <#code#>
     }
 }
