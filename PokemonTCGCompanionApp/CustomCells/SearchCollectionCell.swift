61b1fd2cc438a2e20825f5ddb96780215123ae1b
Curtis Pritchard <cpritchard649@gmail.com>
2020-10-15 21:46:38 -0400

    fixed the collectionView layout to contain 3 per row

---
+++ PokemonTCGCompanionApp/CustomCells/SearchCollectionCell.swift
@@ -0,0 +1,15 @@
+//
+//  SearchCollectionCell.swift
+//  PokemonTCGCompanionApp
+//
+//  Created by Curtis Pritchard on 2020-10-15.
+//
+
+import UIKit
+
+
+class SearchCollectionCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgeView: UIImageView!
+    
+}
---
+++ PokemonTCGCompanionApp/Layouts/SearchGridLayout.swift
@@ -0,0 +1,13 @@
+//
+//  SearchGridLayout.swift
+//  PokemonTCGCompanionApp
+//
+//  Created by Curtis Pritchard on 2020-10-15.
+//
+
+import UIKit
+
+class GridLayout: UICollectionViewLayout {
+        
+}
+
