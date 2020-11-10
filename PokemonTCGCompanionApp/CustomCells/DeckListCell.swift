5765f580373721e777abde3a1037c1207dd83fab
Curtis Pritchard <cpritchard649@gmail.com>
2020-11-10 17:48:48 -0500

    created the look for each indevidual item for the deck list

---
+++ PokemonTCGCompanionApp/CustomCells/DeckListCell.swift
@@ -0,0 +1,16 @@
+//
+//  DeckListCell.swift
+//  PokemonTCGCompanionApp
+//
+//  Created by Curtis Pritchard on 2020-11-10.
+//
+
+import UIKit
+
+class DeckListCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var quantityLabel: UILabel!
+    
+}
---
+++ PokemonTCGCompanionApp/ViewControllers/DeckListController.swift
@@ -0,0 +1,12 @@
+//
+//  DeckListController.swift
+//  PokemonTCGCompanionApp
+//
+//  Created by Curtis Pritchard on 2020-11-10.
+//
+
+import UIKit
+
+class DeckListController: UIViewController {
+    
+}
